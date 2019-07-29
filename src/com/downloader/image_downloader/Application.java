c74a79d0a93ce3a187a012de1ce91520321fd475
Yashith Chanuka <yashith.chanu@gmail.com>
2019-07-29 17:45:22 +0530

    first commit

--- src/com/downloader/image_downloader/Application.java
+++ src/com/downloader/image_downloader/Application.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.akvasoft.image_downloader;
+package com.downloader.image_downloader;
 
 import java.awt.Desktop;
 import java.awt.image.BufferedImage;
